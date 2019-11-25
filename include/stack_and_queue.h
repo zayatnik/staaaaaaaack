c7a12e91a13074fa7574df3f4a61e2d371444da0
zayatnik <55553752+zayatnik@users.noreply.github.com>
2019-11-26 01:59:40 +0300

    засунул реализацию методов очереди в сам класс, переписал саму реализацию, поправил один метод из стэка для удобства применения его в очереди

--- include/stack_and_queue.h
+++ include/stack_and_queue.h
@@ -14,7 +14,7 @@ public:
 	void push(const t& el);
 	int size();
 	bool empty();
-	void pop();
+	int pop();
 	const t& onthetop();
 	bool operator==(stack s);
 	stack& operator=(const stack& s);
@@ -83,7 +83,7 @@ bool stack<t>::empty() {
 }
 
 template<class t>
-void stack<t>::pop() {
+int stack<t>::pop() {
 	if (!(this->empty())) {
 		if ((r = rr / 3) && (r > 8)) {
 			int i;
@@ -107,6 +107,7 @@ void stack<t>::pop() {
 	}
 	else
 		throw "not correct";
+	return cont[r - 1];
 }
 
 template<class t>
@@ -155,125 +156,26 @@ stack<t>& stack<t>::operator=(const stack<t>& s) {
 
 class queue {
 private:
-	int a;
-	stack<int> s1, s2;
+	stack<int> *s1, *s2;
 public:
-	queue();
-	queue(const queue&);
-	void push(int el);
-	void pop();
-	int front();
-	int back();
-	bool pust();
-	queue& operator=(const queue&);
-	int size();
+	queue() {
+		s1 = new stack<int>();
+		s2 = new stack<int>();
+	}
+
+	void push(int el) {
+		s1->push(el);
+	}
+
+	int pop() {
+		if ((s1->empty()) && (s2->empty()))
+			throw "not correct";
+		else {
+			if (s2->empty()) {
+				while (!(s1->empty()))
+					s2->push(s1->pop());
+			}
+			return s2->pop();
+		}
+	}
 };
-
-queue::queue() : s1(), s2() {
-	a = 0;
-}
-
-queue::queue(const queue& q) {
-	a = q.a;
-	s1 = q.s1;
-	s2 = q.s2;
-}
-
-void queue::push(int el) {
-	if (s1.pust() && s2.pust()) {
-		s1.push(el);
-		a = 0;
-	}
-	else {
-		if (a == 0) {
-			int i;
-			int s = s1.size();
-			for (i = 0; i < s; i++) {
-				s2.push(s1.onthetop());
-				s1.pop();
-			}
-			s2.push(el);
-			a = 1;
-		}
-		else
-			s2.push(el);
-	}
-}
-
-void queue::pop() {
-	if (s1.pust() && s2.pust()) {
-		throw "not correct";
-	}
-	else {
-		if (a == 0)
-			s1.pop();
-		else {
-			int i;
-			int s = s2.size();
-			for (i = 0; i < s; i++) {
-				s1.push(s2.onthetop());
-				s2.pop();
-			}
-			s1.pop();
-			a = 0;
-		}
-	}
-}
-
-int queue::front() {
-	if (s1.pust() && s2.pust()) {
-		throw "not correct";
-	}
-	else {
-		if (a == 0)
-			return(s1.onthetop());
-		else {
-			int i;
-			int s = s2.size();
-			for (i = 0; i < s; i++) {
-				s1.push(s2.onthetop());
-				s2.pop();
-			}
-			a = 0;
-			return(s1.onthetop());
-		}
-	}
-}
-
-int queue::back() {
-	if (s1.pust() && s2.pust()) {
-		throw "not correct";
-	}
-	else {
-		if (a == 1)
-			s1.onthetop();
-		else {
-			int i;
-			int s = s1.size();
-			for (i = 0; i < s; i++) {
-				s2.push(s1.onthetop());
-				s1.pop();
-			}
-			s2.onthetop();
-			a = 1;
-		}
-	}
-}
-
-bool queue::pust() {
-	return (s1.pust() && s2.pust());
-}
-
-queue& queue::operator=(const queue& q) {
-	a = q.a;
-	s1 = q.s1;
-	s2 = q.s2;
-	return *this;
-}
-
-int queue::size() {
-	if (a == 0)
-		return s1.size();
-	else
-		return s2.size();
-}
